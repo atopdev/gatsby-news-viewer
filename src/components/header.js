a7ecb5419fc4ca8a0e26c874467881b917992c2d
atopdev <25312831+atopdev@users.noreply.github.com>
2020-02-23 14:19:56 +0800

    update header with authentication

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,51 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { makeStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+import { withFirebase } from './FirebaseContext'
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: '#ffa400',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}))
+
+const Header = ({ siteTitle, firebase }) => {
+  const classes = useStyles()
+
+  const signOut = () => {
+    firebase.auth().signOut()
+  }
+
+  const { currentUser } = firebase.auth()
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            {siteTitle}
+          </Typography>
+          {currentUser && (
+            <Button color="inherit" onClick={signOut}>
+              Log out
+            </Button>
+          )}
+        </Toolbar>
+      </AppBar>
     </div>
-  </div>
-)
+  )
+}
 
-export default Header
+export default withFirebase(Header)
