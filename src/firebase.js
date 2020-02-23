560eed256c050584eca75971cf2864d80de52a14
atopdev <25312831+atopdev@users.noreply.github.com>
2020-02-23 14:18:50 +0800

    update firebase credentials

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,12 @@
 const config = {
-  apiKey: 'AIzaSyDjS6tGpqjAaC0aXQAGZMkjssABbZzTgSo',
-  authDomain: 'awesome-bb71b.firebaseapp.com',
-  databaseURL: 'https://awesome-bb71b.firebaseio.com',
-  projectId: 'awesome-bb71b',
-  storageBucket: 'awesome-bb71b.appspot.com',
-  messagingSenderId: '646496200074',
+  apiKey: process.env.GATSBY_FIREBASE_API_KEY,
+  authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.GATSBY_FIREBASE_DATABASE_URL,
+  projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.GATSBY_FIREBASE_APP_ID,
+  measurementId: process.env.GATSBY_FIREBASE_MEASUREMENT_ID,
 }
 
 let firebaseCache
